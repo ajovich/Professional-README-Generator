ac2aabdd22cd5a7970db8b72006cdfda35067511
Allysha Jovich <ajovich@hotmail.com>
2021-05-17 20:35:27 -0700

    Created an array of questions for user input

--- index.js
+++ index.js
@@ -1,7 +1,65 @@
-// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+inquirer
+  .prompt([
+    {
+      type: "input",
+      name: "project_title",
+      message: "What is the title of your project?",
+    },
+    {
+      type: "input",
+      name: "description",
+      message: "What is the description of your project?",
+    },
+    {
+      // Table of Content Links -- ??? How do I do this?
+      type: "input",
+      name: "bio",
+      message: "What is your bio?",
+    },
+    {
+      type: "input",
+      name: "installation",
+      message: "What are the steps required to install your project? Provide a step-by-step description of how to get your project running.",
+    },
+    {
+      type: "input",
+      name: "usage",
+      message: "What are the instructions and examples for use?",
+    },
+    {
+      type: "input",
+      name: "license",
+      message: "Select the open source license desired from the list below. If none, leave empty.",
+    },
+    {
+      type: "input",
+      name: "contributing",
+      message: "List your collaborators, if any.",
+    },
+    {
+        type: "input",
+        name: "tests",
+        message: "Write tests for your application here.",
+    },
+    {
+        type: "input",
+        name: "questions",
+        message: "Enter your GitHub profile name and e-mail.",
+    },
+  ])
+
+//   .then((response) => {
+//     console.log(response);
+//     const html = generateHTML(response);
+//     const filename = `${response.name.toLowerCase().split(" ").join("")}.html`;
+//     fs.writeFile(filename, html, (err) =>
+//       err ? console.log(err) : console.log("Success!")
+//     );
+//   });
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
